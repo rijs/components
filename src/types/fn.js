f2d9a8d311998cc9a84fccc62141dea1ec5621c3
pemrouz <p3pedram@hotmail.com>
2016-04-08 00:00:15 +0100

    remove extends functionality

--- src/types/fn.js
+++ src/types/fn.js
@@ -8,23 +8,15 @@ export default function fn(ripple) {
 
     var proto = Object.create(HTMLElement.prototype)
       , opts = { prototype: proto }
-      , extend = res.headers['extends']
 
-    extend && (opts.extends = extend)
     proto.attachedCallback = ripple.draw
     document.registerElement(res.name, opts)
   }
 }
 
-function registered(res) {
-  var extend = header('extends')(res)
-    
-  return extend ? document.createElement(extend, res.name).attachedCallback
-                : document.createElement(res.name).attachedCallback
-}
+const registered = res => document.createElement(res.name).attachedCallback
 
 import includes from 'utilise/includes'
-import header from 'utilise/header'
 import client from 'utilise/client'
 import all from 'utilise/all'
 const customs = client && !!document.registerElement
