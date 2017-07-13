b1c6d01256ed79813918defc610000cf8c63a215
pemrouz <p3pedram@hotmail.com>
2017-07-13 21:46:54 +0100

    chore: drop CE v0 support

--- src/types/fn.js
+++ src/types/fn.js
@@ -1,23 +1,6 @@
-// register custom element prototype (render is automatic)
-export default function fn(ripple) {
-  return res => {
-    if (!customs || !customEl(res) || registered(res)) 
-      return all(`${res.name}:not([inert])
-                 ,[is="${res.name}"]:not([inert])`)
-              .map(ripple.draw)  
-
-    var proto = Object.create(HTMLElement.prototype)
-      , opts = { prototype: proto }
-
-    proto.attachedCallback = ripple.draw
-    document.registerElement(res.name, opts)
-  }
-}
-
-const registered = res => document.createElement(res.name).attachedCallback
-
-import includes from 'utilise/includes'
-import client from 'utilise/client'
 import all from 'utilise/all'
-const customs = client && !!document.registerElement
-    , customEl = d => includes('-')(d.name)
+
+// register custom element prototype (render is automatic)
+export default ripple => res => all(`${res.name}`)
+  .map(node => { delete node.render })
+  .map(ripple.draw)
