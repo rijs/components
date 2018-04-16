e7a4edec2a42f8910113159129daa11f885e1d29
pemrouz <p3pedram@hotmail.com>
2018-04-16 22:40:09 +0100

    3.1.13

--- client.bundle.js
+++ client.bundle.js
@@ -80,6 +80,13 @@ var components = (function () {
       };
   }
 
+  var is$1 = /*#__PURE__*/Object.freeze({
+    default: is_1,
+    __moduleExports: is_1
+  });
+
+  var is$2 = ( is$1 && is_1 ) || is$1;
+
   var to = {
       arr: toArray,
       obj: toObject
@@ -94,11 +101,20 @@ var components = (function () {
       function reduce(p, v, i) {
           if (i === 0) 
               { p = {}; }
-          p[is_1.fn(by) ? by(v, i) : v[by]] = v;
+          p[is$2.fn(by) ? by(v, i) : v[by]] = v;
           return p;
       }
       
   }
+  var to_1 = to.arr;
+  var to_2 = to.obj;
+
+  var to$1 = /*#__PURE__*/Object.freeze({
+    default: to,
+    __moduleExports: to,
+    arr: to_1,
+    obj: to_2
+  });
 
   var commonjsGlobal = typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
 
@@ -108,28 +124,52 @@ var components = (function () {
 
   var client = typeof window != 'undefined';
 
-  var owner = client ? window : commonjsGlobal;
+  var client$1 = /*#__PURE__*/Object.freeze({
+    default: client,
+    __moduleExports: client
+  });
+
+  var client$2 = ( client$1 && client ) || client$1;
+
+  var owner = client$2 ? window : commonjsGlobal;
+
+  var owner$1 = /*#__PURE__*/Object.freeze({
+    default: owner,
+    __moduleExports: owner
+  });
+
+  var to$2 = ( to$1 && to ) || to$1;
+
+  var owner$2 = ( owner$1 && owner ) || owner$1;
 
   var log = function log(ns) {
       return function (d) {
-          if (!owner.console || !console.log.apply) 
+          if (!owner$2.console || !console.log.apply) 
               { return d; }
-          is_1.arr(arguments[2]) && (arguments[2] = arguments[2].length);
-          var args = to.arr(arguments), prefix = '[log][' + new Date().toISOString() + ']' + ns;
+          is$2.arr(arguments[2]) && (arguments[2] = arguments[2].length);
+          var args = to$2.arr(arguments), prefix = '[log][' + new Date().toISOString() + ']' + ns;
           args.unshift(prefix.grey ? prefix.grey : prefix);
           return console.log.apply(console, args), d;
       };
   };
 
+  var log$1 = /*#__PURE__*/Object.freeze({
+    default: log,
+    __moduleExports: log
+  });
+
   var ready = function ready(fn) {
       return document.body ? fn() : document.addEventListener('DOMContentLoaded', fn.bind(this));
   };
 
-  var _class = function (definition) { return assign(definition.class ? definition.class : !definition.prototype ? classed(definition) : definition.prototype.render ? definition : definition.prototype.connected ? definition : classed(definition), {
-      raw: definition
-  }); };
+  var ready$1 = /*#__PURE__*/Object.freeze({
+    default: ready,
+    __moduleExports: ready
+  });
+
+  var _class = function (definition) { return assign(definition.class ? definition.class : !definition.prototype ? classed(definition) : definition.prototype.render ? definition : definition.prototype.connected ? definition : classed(definition)); };
   var assign = Object.assign;
-  var classed = function (render) { return (function () {
+  var classed = function (render) { return render.class = render.class || (function () {
           function anonymous () {}
 
           anonymous.prototype.render = function render$1 () {
@@ -138,6 +178,11 @@ var components = (function () {
 
           return anonymous;
       }()); };
+
+  var _class$1 = /*#__PURE__*/Object.freeze({
+    default: _class,
+    __moduleExports: _class
+  });
 
   var promise_1 = promise;
   function promise() {
@@ -150,36 +195,64 @@ var components = (function () {
       return p;
   }
 
+  var promise$1 = /*#__PURE__*/Object.freeze({
+    default: promise_1,
+    __moduleExports: promise_1
+  });
+
   var flatten = function flatten(p, v) {
       if (v instanceof Array) 
           { v = v.reduce(flatten, []); }
       return p = p || [], p.concat(v);
   };
 
+  var flatten$1 = /*#__PURE__*/Object.freeze({
+    default: flatten,
+    __moduleExports: flatten
+  });
+
   var has = function has(o, k) {
       return k in o;
   };
+
+  var has$1 = /*#__PURE__*/Object.freeze({
+    default: has,
+    __moduleExports: has
+  });
+
+  var has$2 = ( has$1 && has ) || has$1;
 
   var def = function def(o, p, v, w) {
       if (o.host && o.host.nodeName) 
           { o = o.host; }
       if (p.name) 
           { v = p, p = p.name; }
-      !has(o, p) && Object.defineProperty(o, p, {
+      !has$2(o, p) && Object.defineProperty(o, p, {
           value: v,
           writable: w
       });
       return o[p];
   };
 
+  var def$1 = /*#__PURE__*/Object.freeze({
+    default: def,
+    __moduleExports: def
+  });
+
+  var promise$2 = ( promise$1 && promise_1 ) || promise$1;
+
+  var flatten$2 = ( flatten$1 && flatten ) || flatten$1;
+
+  var def$2 = ( def$1 && def ) || def$1;
+
   var noop = function () {};
   var emitterify = function emitterify(body, hooks) {
       body = body || {};
       hooks = hooks || {};
-      def(body, 'emit', emit, 1);
-      def(body, 'once', once, 1);
-      def(body, 'off', off, 1);
-      def(body, 'on', on, 1);
+      def$2(body, 'emit', emit, 1);
+      def$2(body, 'once', once, 1);
+      def$2(body, 'off', off, 1);
+      def$2(body, 'on', on, 1);
       body.on['*'] = body.on['*'] || [];
       return body;
       function emit(type, pm, filter) {
@@ -189,7 +262,7 @@ var components = (function () {
               { results.push(call(li[i].isOnce ? li.splice(i--, 1)[0] : li[i], pm)); } }
           for (var i = 0;i < body.on['*'].length; i++) 
               { results.push(call(body.on['*'][i], [type,pm])); }
-          return results.reduce(flatten, []);
+          return results.reduce(flatten$2, []);
       }
       
       function call(cb, pm) {
@@ -231,7 +304,7 @@ var components = (function () {
       
       function observable(parent, opts) {
           opts = opts || {};
-          var o = emitterify(opts.base || promise_1());
+          var o = emitterify(opts.base || promise$2());
           o.i = 0;
           o.li = [];
           o.fn = opts.fn;
@@ -318,6 +391,13 @@ var components = (function () {
       
   };
 
+  var emitterify$1 = /*#__PURE__*/Object.freeze({
+    default: emitterify,
+    __moduleExports: emitterify
+  });
+
+  var emitterify$2 = ( emitterify$1 && emitterify ) || emitterify$1;
+
   var event = function event(node, index) {
       node = node.host && node.host.nodeName ? node.host : node;
       if (node.on) 
@@ -335,7 +415,7 @@ var components = (function () {
               delete node.listeners[o.type];
           }
       };
-      emitterify(node, {
+      emitterify$2(node, {
           on: on,
           off: off
       });
@@ -354,14 +434,21 @@ var components = (function () {
       };
   };
 
-  var client$1 = typeof window != 'undefined';
+  var event$1 = /*#__PURE__*/Object.freeze({
+    default: event,
+    __moduleExports: event
+  });
+
+  var classed$1 = ( _class$1 && _class ) || _class$1;
+
+  var event$2 = ( event$1 && event ) || event$1;
 
   var define = createCommonjsModule(function (module) {
-      var noop = function () {}, HTMLElement = client$1 && window.HTMLElement || (function () {
+      var noop = function () {}, HTMLElement = client$2 && window.HTMLElement || (function () {
           function anonymous () {}
 
           return anonymous;
-      }()), registry = client$1 && window.customElements || {};
+      }()), registry = client$2 && window.customElements || {};
       module.exports = function define(name, component) {
           if (arguments.length == 1) {
               component = name, name = "anon-" + (registry.anon++);
@@ -370,13 +457,13 @@ var components = (function () {
               { return component.wrapper; }
           if (!name.includes('-')) 
               { return; }
-          if (!client$1) 
-              { return wrap(_class(component)); }
+          if (!client$2) 
+              { return wrap(classed$1(component)); }
           var wrapped = registry.get(name);
           if (wrapped) {
-              if (wrapped.class == _class(component)) 
+              if (wrapped.class == classed$1(component)) 
                   { return wrapped; }
-              wrapped.class = _class(component);
+              wrapped.class = classed$1(component);
               var instances = Array.from(document.querySelectorAll(name));
               instances.map(function (node) {
                   node.disconnectedCallback();
@@ -386,28 +473,29 @@ var components = (function () {
                   node.connectedCallback();
               });
           } else {
-              registry.define(name, wrapped = wrap(_class(component)));
+              registry.define(name, wrapped = wrap(classed$1(component)));
           }
           return wrapped;
       };
       var wrap = function (component) {
           if (!component.hasOwnProperty('wrapper')) 
               { component.wrapper = (function (HTMLElement) {
-                      function undefined() {
-                  HTMLElement.call(this);
-                  event(this);
-                  this.ready = this.once('ready');
-                  this.state = this.state || {};
-              }
+                      function undefined () {
+                          HTMLElement.apply(this, arguments);
+                      }
 
                       if ( HTMLElement ) undefined.__proto__ = HTMLElement;
                       undefined.prototype = Object.create( HTMLElement && HTMLElement.prototype );
                       undefined.prototype.constructor = undefined;
-              undefined.prototype.connectedCallback = function connectedCallback () {
+
+                      undefined.prototype.connectedCallback = function connectedCallback () {
                   var this$1 = this;
 
                   var ref = component.wrapper.class;
                   var prototype = ref.prototype;
+                  event$2(this);
+                  this.ready = this.once('ready');
+                  this.state = this.state || {};
                   this.methods = Object.getOwnPropertyNames(prototype).filter(function (method) { return !(method in disallowed); }).map(function (method) { return (this$1[method] = prototype[method].bind(this$1), method); });
                   return Promise.resolve((this.connected || noop).call(this, this, this.state)).then(function (d) {
                       this$1.emit('ready');
@@ -447,22 +535,33 @@ var components = (function () {
       registry.anon = registry.anon || 1;
   });
 
+  var define$1 = /*#__PURE__*/Object.freeze({
+    default: define,
+    __moduleExports: define
+  });
+
+  var require$$0 = ( log$1 && log ) || log$1;
+
+  var ready$2 = ( ready$1 && ready ) || ready$1;
+
+  var define$2 = ( define$1 && define ) || define$1;
+
   var components = function components(ripple) {
-      if (!client) 
+      if (!client$2) 
           { return ripple; }
-      log$1('creating');
+      log$2('creating');
       Node.prototype.render = function () {
           var name = this.nodeName.toLowerCase();
           if (name.includes('-')) 
-              { return this.fn$ = this.fn$ || ripple.subscribe(name).map(function (component) { return define(name, component); }); }
+              { return this.fn$ = this.fn$ || ripple.subscribe(name).map(function (component) { return define$2(name, component); }); }
       };
       Node.prototype.draw = function () {
           this.render();
       };
-      ready(function () { return Array.from(document.querySelectorAll('*')).filter(function (d) { return d.nodeName.includes('-'); }).map(function (node) { return node.render(); }); });
+      ready$2(function () { return Array.from(document.querySelectorAll('*')).filter(function (d) { return d.nodeName.includes('-'); }).map(function (node) { return node.render(); }); });
       return ripple;
   };
-  var log$1 = log('[ri/components]');
+  var log$2 = require$$0('[ri/components]');
 
   return components;
 
